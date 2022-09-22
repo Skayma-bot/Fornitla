e77abf0c468e1ad429cb8b875ed459d376838cf7
lepetit.mael <lepetit.mael@educ-valadon-limoges.fr>
2022-09-22 16:10:52 +0200

    Android

--- app/src/main/java/com/example/fornitla/MainActivity.java
+++ app/src/main/java/com/example/fornitla/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.fornitla;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mEditText;
+    private TextView mTextView;
+    private Button mButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditText= findViewById(R.id.edit_text);
+        mTextView =findViewById(R.id.textview);
+        mButton= findViewById(R.id.button);
+        mButton.setEnabled(false);
+        mEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                mButton.setEnabled(!editable.toString().isEmpty());
+
+            }
+        });
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    
+            }
+        });
+
     }
 }
